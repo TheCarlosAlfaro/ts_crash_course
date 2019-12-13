88c99c3bd57a60c9c912b2e51d1942d56f58a0fb
devbysalas <rgkalin@gmail.com>
2019-12-13 14:26:27 -0800

    interfaces

---
+++ functions.js
@@ -0,0 +1,5 @@
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+var result = getSum(2, 2);
+console.log(result);
---
+++ functions.ts
@@ -0,0 +1,6 @@
+function getSum(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const result = getSum(2, 2);
+console.log(result);
